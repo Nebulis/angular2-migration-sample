937b7df0816a9925b38b11d23d21e5d66c8375de
Laurent Maillet <laurent.maillet@zenika.com>
2016-01-21 21:50:08 +0100

    refactor step 2

--- step/step2/components/beerItem.directive.js
+++ step/step2/components/beerItem.directive.js
@@ -6,14 +6,7 @@ angular.module('Brewery.directive', []).directive('beerItem', ['BreweryService',
 		}, 
 		controllerAs: 'bi',
 		bindToController: true,
-		template: '<div class="col-md-4 panel panel-default">' +
-				'<div class="panel-heading">' +
-					'<span ng-class="bi.beer.note | NoteFilter"></span>{{bi.beer.name}}</div>' +
-					'<div class="panel-body">' +
-						'{{bi.beer.description}}' +
-					'</div>' +
-					'<button ng-click="bi.selectBeer()" class="btn btn-primary">Give me a pinte !</button>' +
-				'</div>',
+		templateUrl: 'components/beer-item.html',
 		controller: function(){
 			this.selectBeer = function(){
 				BreweryService.getOnePinte(this.beer);
